b672512d654fa5acf382a18e366524cb0b7d756a
sirjan singh <sirjansingh310@gmail.com>
2019-04-07 17:47:12 +0530

    extract snippet from wikipedia

--- app/src/main/java/com/google/firebase/codelab/image_labeling/ImageLabelAdapter.kt
+++ app/src/main/java/com/google/firebase/codelab/image_labeling/ImageLabelAdapter.kt
@@ -13,23 +13,60 @@ import kotlinx.android.synthetic.main.item_row.view.*
 import androidx.core.content.ContextCompat.startActivity
 import android.content.Intent
 import android.net.Uri
-
+import org.jetbrains.anko.custom.async
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.doAsyncResult
+import java.net.*
+import java.io.*
+import java.util.regex.*
 
 class ImageLabelAdapter(private var firebaseVisionList: List<Any>) : RecyclerView.Adapter<ImageLabelAdapter.ItemHolder>() {
     lateinit var context: Context
     inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun extractInfo(inputLabel: String) : String{
+
+            try
+            {
+                    var label = inputLabel
+                    var length = label.length
+                    var testLine = ""
+                    label = label.replace((" ").toRegex(), "%20")
+                      var inputLine = ""
+                      val url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + label + "&namespace=0%7C4&limit=1&profile=classic&redirects=resolve"
+                      val wiki = URL(url)
+                      val `in` = BufferedReader(InputStreamReader(wiki.openStream()))
+                      inputLine = `in`.readLine()
+                      testLine = inputLine.substring(length + 6, inputLine.indexOf("\"", length + 6))
+                      val start = 10 + length + testLine.length
+                      inputLine = inputLine.substring(start, inputLine.indexOf("]", start))
+                      inputLine = inputLine.replace((Matcher.quoteReplacement("\\\"")).toRegex(), Matcher.quoteReplacement("\""))
+                      return inputLine
+
+            }
+
+            catch (e:Exception) {
+                   Toast.makeText(context, e.toString(), Toast.LENGTH_LONG).show()
+            }
+            return ""
+
+        }
+
         fun bindCloud(currentItem: FirebaseVisionCloudLabel) {
             val isExpanded = HashMap<String,Boolean>(firebaseVisionList.size)
             itemView.setOnClickListener(View.OnClickListener {
                 if(isExpanded[currentItem.label] == false) {
-                    itemView.wikiInfo.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                    doAsyncResult {
+                        itemView.wikiInfo.text = extractInfo(currentItem.label)
+                    }
+                    Thread.sleep(1000)
                     itemView.wikiInfo.visibility = View.VISIBLE
                     itemView.wikiButton.visibility = View.VISIBLE
-                    isExpanded[currentItem.label] = true
-                    itemView.wikiButton.setOnClickListener {
-                        val modifiedLabel = currentItem.label.replace(" ","_")
-                        val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://en.wikipedia.org/wiki/"+modifiedLabel))
-                        startActivity(context,i,null)
+                        isExpanded[currentItem.label] = true
+                        itemView.wikiButton.setOnClickListener {
+                            val modifiedLabel = currentItem.label.replace(" ", "_")
+                            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://en.wikipedia.org/wiki/" + modifiedLabel))
+                            startActivity(context, i, null)
                     }
                 }
                 else{
